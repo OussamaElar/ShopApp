0444c8597821498778970a414a0a8b126be0c951
developer <developer@developersair2.mynetworksettings.com>
2022-05-12 12:24:18 -0400

    working on cart

--- ShopApp/View/CartCell.swift
+++ ShopApp/View/CartCell.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class CartCell: UITableViewCell {
+class CartCell: UITableViewCell, UITableViewDelegate, UITableViewDataSource {
+    
+    var items: [Product] = []
+    
+    @objc func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    @objc(tableView:cellForRowAtIndexPath:) func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell") else {
+            return UITableViewCell()
+        }
+        let item = items[indexPath.row]
+        cell.textLabel?.text = "\(item)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tapping cell \(indexPath.section) \(indexPath.row)")
+    }
+
+    @IBOutlet weak var tableView: UITableView!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+    }
+    
     
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
 
 }
