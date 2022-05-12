670a0ba90fded09d335c7d289eb20a110d5ec329
OussamaElar <developer@developers-MacBook-Air-5.local>
2022-05-12 14:40:50 -0400

    savedProduct in progress

---
+++ ShopApp/Controller/SavedProductsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SavedProductsViewController.swift
+//  ShopApp
+//
+//  Created by developer on 5/12/22.
+//
+
+import UIKit
+
+class SavedProductsViewController: UIViewController {
+    
+    
+    var 
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShopApp/View/SavedProductCell.swift
@@ -0,0 +1,23 @@
+//
+//  SavedProductCell.swift
+//  ShopApp
+//
+//  Created by developer on 5/12/22.
+//
+
+import UIKit
+
+class SavedProductCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
