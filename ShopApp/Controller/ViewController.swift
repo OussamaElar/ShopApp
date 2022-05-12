7ef0d08f47763b3a285a949512166c53d80c517f
OussamaElar <developer@developers-MacBook-Air-5.local>
2022-05-12 09:37:11 -0400

    fetching data done

--- ShopApp/Controller/ViewController.swift
+++ ShopApp/Controller/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     deinit {
         print("TV Deinit")
     }
-    
+    // 
     func getData() {
         WebService.shared.getProducts { products in
             self.products = products
